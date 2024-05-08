c922ebccf096f6e5497b79076dd3ad062460ffe8
Jana Shv <janashvets@gmail.com>
2024-05-08 20:57:24 +0300

    fdsfsf

---
+++ info.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "адрес_вашего_MySQL_сервера";
+$username = "DBlietotajs";
+$password = "Pa$$word1234";
+$database = "UzvardsDB";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$sql = "CREATE TABLE VirtM (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    название_столбца VARCHAR(30) NOT NULL,
+    еще_один_столбец VARCHAR(30) NOT NULL,
+    и_так_далее VARCHAR(50)
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Таблица VirtM успешно создана";
+} else {
+    echo "Ошибка при создании таблицы: " . $conn->error;
+}
+
+$conn->close();
+?>
